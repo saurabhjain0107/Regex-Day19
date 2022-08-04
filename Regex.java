78fcb6ba76e773a5dc73e17f61e4fdcebf6957a0
saurabh jain <luckyjain0107@gmai.com>
2022-08-04 18:18:22 +0530

    Last Name

--- Regex.java
+++ Regex.java
@@ -1,11 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    public void isFirstName(String FirstName) {
+    public void isFirstName() {
         Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first name :");
+        String FirstName = sc.nextLine();
         Matcher matcher = pattern.matcher(FirstName);
         if (matcher.matches()) {
             System.out.println("First name is valid : ");
@@ -15,10 +19,24 @@ public class Regex {
 
         }
     }
+    public  void IsLastName(){
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter last name :");
+        String lastname = sc.nextLine();
+        Matcher matcher = pattern.matcher(lastname);
+        if(matcher.matches()){
+            System.out.println("last name in valid : ");
+        }
+        else{
+            System.out.println("last name is Invalid : ");
+        }
+    }
 
     public static void main(String[] args) {
         Regex obj = new Regex();
-        obj.isFirstName("Saurabh");
+        obj.isFirstName();
+        obj.IsLastName();
 
     }
 
